8ad8572a7a338e219fa407313141c639f8eb46ce
Taco Miguel <miguel.taco@unmsm.edu.pe>
2025-10-24 17:52:45 -0500

    Se agregó tipografia Raleway para los titulos e Inter para el texto, se terminaron las maquetaciones para cada pagina, falta la navefacion y el logo

--- ihc-frontend/src/pages/MainPage.jsx
+++ ihc-frontend/src/pages/MainPage.jsx
@@ -1,14 +1,30 @@
 import '../styles/MainPage.css'
 import ItemMenu from '../components/ItemMenu';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons'
+import logo from '../assets/imgs/logo.png'
 function MainPage(){
     return(
         <div className="main-page-container">
-            <div className='mainpage-head-container'>
-                <h1>MENU DEL DÍA</h1>
-                <div className='buttons-main-container'>
-
+            <div className='mainpage-head-container row'>   
+                <div className='head-left-top-container row'>
+                    <h1>SMARTFOOD</h1>
+                    {/*<img class="logo-smartFood" src="" alt="logo"></img>*/}
                 </div>
+                <div className='head-center-top-container'>
+                    <h1>MENU DEL DÍA</h1>
+                    <div className='buttons-main-container row'>
+                        <button className='button-general'>Crear perfil</button>
+                        <button className='button-general'>Ver perfiles</button>
+                        <button className='button-general'>Seleccionar perfil</button>
+                    </div>
+                </div>
+                <div className='head-right-top-container col'>
+                    <FontAwesomeIcon className="user-login-icon" icon={faCircleUser} />
+                    <button>Login</button>
+                    <button>Register</button>
+                </div>
+
             </div>
             <div className='mainpage-body-container row'>
                 <aside className="filtros-container">
